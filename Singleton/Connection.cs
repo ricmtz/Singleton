18837e1c0451f55df22eb4828692d0cf09092182
ricmtz <ricmtz15@gmail.com>
2019-03-21 16:12:46 -0600

    Add project files.

---
+++ Singleton/Connection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Connection
+    {
+        private static Connection connection;
+        private static Object _lock = new Object();
+
+        private string server;
+
+        private Connection() {
+            this.server = "SERVER//ACDC";
+        }
+
+        public static Connection GetInstance() {
+            lock (_lock) {
+                return connection ?? (connection = new Connection());
+            }
+        }
+
+        public String Status() {
+            return "OK";
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Connection conn1 = Connection.GetInstance();
+            Connection conn2 = Connection.GetInstance();
+
+            if(conn1 == conn2) {
+                Console.WriteLine("conn1 and conn2 are the same");
+            } else {
+                Console.WriteLine("Diferent");
+            }
+
+            Console.WriteLine("Connection 1: {0}", conn1.Status());
+            Console.WriteLine("Connection 2: {0}", conn2.Status());
+
+            Console.ReadKey(true);
+        }
+    }
+}
